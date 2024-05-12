316560e4821f36003937eb801d411f318c9ef99f
K M Rejowan Ahmmed <ahmmedrejowan@gmail.com>
2024-05-12 21:04:29 +0600

    Jitpack Fix

--- ccpc/build.gradle.kts
+++ ccpc/build.gradle.kts
@@ -44,7 +44,7 @@ android {
 
 dependencies {
 
-    implementation(libs.libphonenumber.android)
+    api(libs.libphonenumber.android)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
