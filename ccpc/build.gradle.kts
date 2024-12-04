ec225854a5bcd4df168c585b16ffde3df59bd2da
Nazar Lenyk <nazar.lenyk@vanongo.com>
2024-12-04 16:58:46 +0200

    chore: update artifact and app version to 0.1.3

--- ccpc/build.gradle.kts
+++ ccpc/build.gradle.kts
@@ -71,7 +71,7 @@ afterEvaluate {
                 from(components.getByName("release"))
                 groupId = "com.rejowan"
                 artifactId = "ccpc"
-                version = "0.1.2"
+                version = "0.1.3"
             }
         }
     }
