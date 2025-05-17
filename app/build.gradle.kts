722d4d5034ec649f9fa15aa4a97612f49ac49ea4
Abbass Sharara <abbass.sharara.as@gmail.com>
2025-05-17 19:19:00 +0200

    Clean code more Add findCountries to reutrn one single country Make searchCountries return multiple countries instead one single country.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 2
-        versionName = "0.1.6"
+        versionName = "0.1.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
--- ccpc/build.gradle.kts
+++ ccpc/build.gradle.kts
@@ -71,7 +71,7 @@ afterEvaluate {
                 from(components.getByName("release"))
                 groupId = "com.rejowan"
                 artifactId = "ccpc"
-                version = "0.1.6"
+                version = "0.1.7"
             }
         }
     }
